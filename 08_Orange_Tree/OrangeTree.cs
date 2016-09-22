ad106c48cbf233a8eb086e596ec0c62783cc2690
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-09-22 08:30:30 +0200

    Add project files.

---
+++ 08_Orange_Tree/OrangeTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08_Orange_Tree
+{
+    internal class OrangeTree
+    {
+        private int v1;
+        private int v2;
+
+        public OrangeTree(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return Age;
+            }
+            internal set
+            {
+
+            }
+        }
+
+        internal void OneYearPasses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
